79d3961fa93d2a83eed1257e19464fbe8aad5acf
LikaShu <142459648+LikaShu@users.noreply.github.com>
2024-05-07 21:01:47 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+main();
+    data.forEach(item => {
+
+
+        console.log("Random Number: " + item);
+function main() {
+    const data = [];
+function generateRandomData() {
+    }
+}
+
+    const data = generateRandomData();
+    return data;
+    });
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    data.forEach(item => {
+
+
+        console.log("Random Number: " + item);
+    }
+    const data = generateRandomData();
+main();
+function main() {
+    const data = [];
+function generateRandomData() {
+
+    return data;
+}
+    for (let i = 0; i < 10; i++) {
+    });
